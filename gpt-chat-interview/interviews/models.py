224dea447002635c09666d093f3989fa47879dd4
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2023-12-22 13:09:10 -0300

    feat: gpt conversation

--- gpt-chat-interview/interviews/models.py
+++ gpt-chat-interview/interviews/models.py
@@ -1,6 +1,9 @@
 import uuid
 
 from django.db import models
+from django.conf import settings
+
+from .services import GptService
 
 
 class Chat(models.Model):
@@ -16,6 +19,16 @@ class Chat(models.Model):
         if not self.uuid:
             self.uuid = uuid.uuid4()
             self.title = f"Chat {self.job.title} - {self.uuid}"
+            super().save(*args, **kwargs)
+            initial_prompt = settings.INITIAL_PROMPT_TEMPLATE
+            initial_prompt = initial_prompt.replace("{job_title}", self.job.title)
+            initial_prompt = initial_prompt.replace("{job_requirements}", self.job.requirements)
+            initial_prompt = initial_prompt.replace("{job_responsibilities}", self.job.responsibilities)
+            Message.objects.create(
+                chat=self,
+                role="system",
+                content=initial_prompt
+            )
         super().save(*args, **kwargs)
 
 
@@ -33,3 +46,25 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.role} - {self.chat.title}"
+
+    def save(self, *args, **kwargs):
+        if not self.pk and self.role != "assistant" and not self.chat.completed:
+            super().save(*args, **kwargs)
+
+            if self.role == "user" and self.chat.messages.filter(role="assistant").count() >= 5:
+                Message.objects.create(
+                    chat=self.chat,
+                    role="system",
+                    content="Realize o feedback do candidato, esse feedback deve informar quais foram os pontos positivos, os pontos negativos, o que pode ser melhorado e se o candidato possui um perfil adequado para a vaga."
+                )
+                self.chat.completed = True
+                self.chat.save()
+            else:
+                service = GptService()
+                Message.objects.create(
+                    chat=self.chat,
+                    role="assistant",
+                    content=service.get_chat_completion(self.chat.messages.all())
+                )
+        else:
+            super().save(*args, **kwargs)
